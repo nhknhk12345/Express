606ecb987fd93702e17753a9d80f7055854c4371
nhknhk12345 <ngohoangkiet789@gmail.com>
2025-03-14 10:28:35 +0700

    first commit

---
+++ schemas/category.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+let categorySchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description:{
+        type: String,
+        default: "",
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false,
+    }
+      
+},{
+    timestamps:true
+})
+module.exports = mongoose.model('Category',categorySchema);
---
+++ schemas/product.js
@@ -0,0 +1,33 @@
+let mongoose = require('mongoose');
+
+let productSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },quantity:{
+        type:Number,
+        min:0,
+        required:true
+    },price:{
+        type:Number,
+        min:0,
+        required:true
+    },description:{
+        type:String,
+        default:""
+    },imgURL:{
+        type:String,
+        default:""
+    },category:{
+        type:String,
+        required:true
+    }
+    ,isDeleted:{
+        type:Boolean,
+        default:false
+    }
+},{
+    timestamps:true
+})
+module.exports = mongoose.model('product',productSchema)
